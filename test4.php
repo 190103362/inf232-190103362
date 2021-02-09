d1de85fd1c68154dea12c5edced17ce78f7d516c
190103362 <190103362@stu.sdu.edu.kz>
2021-02-09 21:13:15 +0600

    4

---
+++ test4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$txt = "Отан - оттан да ыстық!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+ECHO "<br>";
+echo $x;
+Echo "<br>";
+echo  $y;
+?>
+
+</body>
+</html>
