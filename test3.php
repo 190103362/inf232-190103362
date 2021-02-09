befcc256a9f02312b319e40b9fd51560b7f37750
190103362 <190103362@stu.sdu.edu.kz>
2021-02-09 21:12:40 +0600

    3

---
+++ test3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$COLOR = " red ";
+$color = " green ";
+echo "My car is" . $COLOR . "<br>";
+ECHO "My house is" . $color . "<br>";
+eCho  "My boat is". $color ."<br>";
+
+// This is a single-line comment 
+
+# This is also a single-line comment 
+
+/* 
+This is a multiple-lines comment block 
+that spans over multiple lines 
+
+*/
+
+?>
+
+</body>
+</html>
+
+
